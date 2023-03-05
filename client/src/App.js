43103fe2e9e32de6c10eeb49b497610ff1bbc442
Vishaal Agartha <vishaalagartha@gmail.com>
2023-03-05 11:11:47 -0800

    Add eslint and prettier to package

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,34 @@
-import { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 const App = () => {
   useEffect(() => {
     fetch('/api/', {
       method: 'GET',
-    }).then(res => res.json())
-    .then(console.log)
+    })
+      .then((res) => res.json())
+      .then(console.log)
   }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
