5b92c06ae749ee20c935c7b523c12a7809caede6
okapreS2023 <okapre@andrew.cmu.edu>
2023-03-25 00:41:23 -0700

    refactor: removed unnecessary try catch block in client side profile api

--- client/src/api/userProfile.js
+++ client/src/api/userProfile.js
@@ -1,42 +1,21 @@
 import request from '../utils/request'
 
-export const updatePersonalIdentityAPI = async (payload) => {
-  try {
-    const response = await request('users/personal-identity', {
-      method: 'PUT',
-      body: JSON.stringify(payload),
-    })
-    return response
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
-}
+export const updatePersonalIdentityAPI = async (payload) =>
+  request('users/personal-identity', {
+    method: 'PUT',
+    body: JSON.stringify(payload),
+  })
 
-export const fetchUserAPI = async () => {
-  try {
-    const response = await request('users', {
-      method: 'GET',
-    })
-    return response
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
-}
+export const fetchUserAPI = async () =>
+  request('users', {
+    method: 'GET',
+  })
 
-export const updatePersonalInformationAPI = async (payload) => {
-  try {
-    const response = await request('users/personal-information', {
-      method: 'PUT',
-      body: JSON.stringify(payload),
-    })
-    return response
-  } catch (error) {
-    console.error(error)
-    throw error
-  }
-}
+export const updatePersonalInformationAPI = async (payload) =>
+  request('users/personal-information', {
+    method: 'PUT',
+    body: JSON.stringify(payload),
+  })
 
 export const addSkillAPI = async (payload) =>
   request('users/skills', {
